ea2972fdf8a9709b0913f7ab4a810632a107e08a
zDein <carlosantonioeyes@gmail.com>
2024-04-26 17:34:26 -0300

    Atualizando o repositório

---
+++ atividade08/src/main/java/Vetor.java
@@ -0,0 +1,11 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Vetor {
+    private int[] vetor = new int[10];
+    private int count = 0;
+
+    public void preencherVetor() {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
