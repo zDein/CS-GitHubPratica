27307c7f5a02f361ec148e7de7f62e73a3fbbbfd
zDein <carlosantonioeyes@gmail.com>
2024-04-26 17:41:12 -0300

    Atualizando o repositório

--- atividade08/src/main/java/Vetor.java
+++ atividade08/src/main/java/Vetor.java
@@ -7,16 +7,23 @@ public class Vetor {
 
     public void preencherVetor() {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Digite um número inteiro (0 para parar):");
+                int numero = scanner.nextInt();
 
-    while (true) {
-            System.out.println("Digite um número inteiro (0 para parar):");
-            int numero = scanner.nextInt();
+                vetor[count] = numero;
+                count++;
 
-            vetor[count] = numero;
-            count++;
-
-            if (numero == 0) {
+                if (numero == 0) {
+                    break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Erro: Você digitou mais de 10 valores.");
                 break;
+            } catch (InputMismatchException e) {
+                System.out.println("Erro: Você digitou um valor que não é numérico.");
+                scanner.next(); // limpa o buffer do scanner
             }
         }
     }
